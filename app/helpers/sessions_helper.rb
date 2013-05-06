b028591b7bc3fc3f50e2e95d4f0ac30c5f7c4a9e
LinhNT <linhnt@linhnt-pc.(none)>
2013-05-06 10:31:08 +0700

    update draft layout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,36 @@
 module SessionsHelper
   def sign_in user
-      self.current_user = user
+    session[:user_id] = user.id
+    self.current_user = user
   end
 
-  def current_user= user
+  def current_user=(user)
     @current_user = user
   end
   
   def current_user
-    @current_user
+    @current_user ||= User.find_by_id(session[:user_id])
   end
   
   def sign_out
+    session.delete :user_id
     self.current_user = nil
+  end
+  
+  def signed_in?
+    !current_user.nil?
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
   end
 end
