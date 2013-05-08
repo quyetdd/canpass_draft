81cd903b3d6f2de2cc11faee188e2acef0e58f8f
LinhNT <linhnt@linhnt-pc.(none)>
2013-05-08 10:43:18 +0700

    add multi language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   
+  before_filter :set_locale
+  
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
   
   def must_super
     redirect_to(root_path) unless current_user.role_id == 1  
+  end
+  
+  private
+  def set_locale
+    I18n.locale = cookies[:locale] || I18n.default_locale
   end
 end
