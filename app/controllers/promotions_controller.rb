f5b6dc4e3fce3efd795c231f99ce28f68bc3fc39
bangsep <kbang.vk@gmail.com>
2013-05-14 16:21:32 +0700

    update highchart

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -7,8 +7,13 @@ class PromotionsController < ApplicationController
   def show
     #@promotion = Promotion.find(params[:id])
     @chart = LazyHighCharts::HighChart.new('graph') do |f|
-    f.series(:type=> 'spline',:name=> 'Clicks', :data=> [300, 200, 300, 0, 500])
-	f.series(:type=> 'spline',:name=> 'Imp', :data=> [200, 0, 200, 500, 400], :color => 'yellow')
+    f.series(:type=> 'spline',:name=> 'Clicks', :data=> [300, 200, 300, 0, 500, 350, 250, 270, 280, 260, 262, 265])
+	f.series(:type=> 'spline',:name=> 'Imp', :data=> [200, 0, 200, 500, 400, 450, 420, 350, 240, 230, 211, 245], :color => 'green')
+	f.legend(:align => "right", :verticalAlign => "top", :y => 0, :x => -50, :layout => 'vertical', :borderWidth => 0)
+	f.xAxis(:categories => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			:labels => {:style => {:color => '#6D869F', :font => '12px Helvetical'}},
+			)
+	f.yAxis(:min => 0, :title => '')
 	end
   end
 
